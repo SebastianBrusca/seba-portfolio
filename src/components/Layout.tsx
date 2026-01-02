9a4453d49c254fd0662772568c0b351d4d7feb5c
Sebastian Brusca <sebastian_brusca@hotmail.com>
2026-01-02 14:22:32 -0300

    Remove Navbar from shared Layout component; keep global layout only

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Navbar from "./Navbar";
-
 export default function Layout({
   children,
 }: {
@@ -9,7 +7,7 @@ export default function Layout({
 }) {
   return (
     <div className="min-h-screen text-neutral-100 relative z-10">
-      <Navbar />
+      <>
       <main className="mx-auto flex max-w-6xl flex-col gap-8 px-4 py-8">
         {children}
       </main>
